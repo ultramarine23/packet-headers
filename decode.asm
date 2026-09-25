abe6743145d0f087067b165120bbd59cad716e64
Jared Chua <jaredrchua@gmail.com>
2026-09-25 21:23:50 +0800

    feat: implement decode functionality
    decode now fully functional besides checksum field (which requires the checksum functionality)

--- decode.asm
+++ decode.asm
@@ -62,6 +62,126 @@ _decode_header:
         ; the struct, and driver.c does the rest.
         ;
 
+        mov     esi, [ebp + 8]
+        mov     edi, [ebp + 12]
+
+        ; move the zeroth byte into ebx {y}
+        movzx   ebx, byte [esi + 0]
+
+        ; decode version
+        ; ignore lower 4 bits and shift to the beginning
+        mov     eax, ebx
+        and     eax, 0b11110000
+        shr     eax, 4
+        mov     [edi], eax
+
+        ; decode IHL
+        ; ignore upper 4 bits, and no need to shift
+        mov     eax, ebx
+        and     eax, 0b00001111
+        mov     [edi + 4], eax
+
+
+        ; move the first byte into ebx {y}
+        movzx   ebx, byte [esi + 1]
+
+        ; decode dscp
+        ; ignore lower 2 bits and shift to the beginning
+        mov     eax, ebx
+        and     eax, 0b11111100
+        shr     eax, 2
+        mov     [edi + 8], eax
+
+        ; decode ECN
+        ; ignore upper 6 bits, and no need to shift
+        mov     eax, ebx
+        and     eax, 0b00000011
+        mov     [edi + 12], eax
+
+
+        ; move and process the second/third bytes {y}
+        ; decode length
+        ; big-endian: byte 2 goes on ah, byte 3 goes on al
+        ; eax naturally reverses the two
+        xor     eax, eax
+        mov     ah, [esi + 2]
+        mov     al, [esi + 3]
+        mov     [edi + 16], eax
+
+
+        ; move and process the fourth/fifth bytes {y}
+        ; decode identification
+        ; big-endian: byte 4 goes on ah and byte 5 goes on al
+        mov     ah, [esi + 4]
+        mov     al, [esi + 5]
+        mov     [edi + 20], eax
+
+
+        ; move and process the six/seventh bytes {y}
+        xor     eax, eax
+        xor     ebx, ebx
+        mov     bl, [esi + 6]
+        mov     bh, [esi + 7]
+
+        ; decode flags
+        ; take the top 2-3 bits and shift to the beginning
+        mov     al, bl
+        and     al, 0b01100000
+        shr     al, 5
+        mov     [edi + 24], eax
+
+        ; decode fragment offset
+        ; exploit ah-al by processing byte 6 on ah and letting
+        ; al copy byte 7
+        mov     ah, bl
+        and     ah, 0b00011111
+        mov     al, bh
+
+        mov     [edi + 28], eax
+
+
+        ; move and process the eigth byte {y}
+        xor     eax, eax
+        mov     al, [esi + 8]
+        mov     [edi + 32], eax
+
+
+        ; move and process the ninth byte {y}
+        mov     al, [esi + 9]
+        mov     [edi + 36], eax
+
+
+        ; move and process the tenth/eleventh byte {y}
+        mov     ah, [esi + 10]
+        mov     al, [esi + 11]
+        ; call checksum function or whatever, here
+        mov     [edi + 40], eax
+
+
+        ; move and process the twelfth-fifteenth byte {y}
+        xor     eax, eax
+        xor     ebx, ebx
+        mov     al, [esi + 12]
+        mov     ah, [esi + 13]
+        mov     bl, [esi + 14]
+        mov     bh, [esi + 15]
+        shl     ebx, 16
+        or      eax, ebx
+        mov     [edi + 44], eax
+
+
+        ; move and process the sixteenth-nineteenth byte {y}
+        xor     eax, eax
+        xor     ebx, ebx
+        mov     al, [esi + 16]
+        mov     ah, [esi + 17]
+        mov     bl, [esi + 18]
+        mov     bh, [esi + 19]
+        shl     ebx, 16
+        or      eax, ebx
+        mov     [edi + 48], eax
+
+
         popa
         mov     eax, 0
         leave
